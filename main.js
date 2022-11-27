ae537dce95ced2ac78dedefa2094b1bc372a8431
OkamiSuarez <okami97.backdev@hotmail.com>
2022-11-27 17:55:13 -0600

    Se genera codigo js para button de form

--- main.js
+++ main.js
@@ -3,3 +3,31 @@
 function downloadCV() {
     alert('Downloading')
 };
+
+function formButton(){
+    let form = document.getElementById('mailButton');
+    let openedForm = document.getElementById('openedMailButton');
+    let formContainer = document.getElementById('contactFormContainer');
+        if (form) {
+            alert('es el button')
+            form.innerHTML = ('funciona');
+            form.id = "openedMailButton";
+            // Aquí se mete la función del form
+                createForm(formContainer);
+        } else if (openedForm){
+            alert('es el form');
+            openedForm.innerHTML = ('Funciona de regreso');
+            openedForm.id = ('mailButton');
+        }
+        else {
+            alert('id missmatch found')
+        }
+};
+
+function createForm(formVar){
+    let formDesign = document.createElement(`div`);
+    formDesign.id = "pruebaDeId";
+    formDesign.classList.add('pruebaDeClass');
+    formDesign.innerHTML = 'infoinfoinfo';
+    formVar.appendChild(formDesign);
+}
